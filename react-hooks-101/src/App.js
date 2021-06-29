e66bce854c890dac765df94301b365d07af83de7
seiya-1208 <www.dokomo.co.jp@gmail.com>
2021-06-29 12:04:20 +0900

    implement callbacks with useEffect

--- react-hooks-101/src/App.js
+++ react-hooks-101/src/App.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const App = (props) => {
   // オブジェクトはそのまま渡すことが可能
   const [state, setState] = useState(props);
   const { name, price } = state;
+
+  // useEffectは、レンダリングの後で実行されている
+  useEffect(() => {
+    console.log("This is like componentDidMount or componentDidUpdate");
+  });
+
+  useEffect(() => {
+    console.log("This is like componentDidMount");
+  }, []);
+
+  useEffect(() => {
+    console.log("This callback is for name only");
+  }, [name]);
 
   return (
     <React.Fragment>
